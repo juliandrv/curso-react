0a0088b23747a0f856d036553f287261ab7ec046
Julián Ramírez <judramirezva@gmail.com>
2021-03-17 14:18:15 -0500

    CounterApp project

---
+++ 03.counter-app/src/CounterApp.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { PropTypes } from "prop-types";
+
+
+
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(value);
+
+  // handleAdd
+  const handleAdd = () => {
+    // setCounter((c) => c + 1);
+    setCounter(counter + 1);
+  }
+
+  // handleSubstract
+  const handleSubstract = () => {
+    setCounter(counter - 1);
+  }
+
+  // handleReset
+  const handleReset = () => {
+    setCounter(value);
+  }
+
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <div>
+        <button onClick={handleSubstract}>- 1</button>
+        <h2> {counter} </h2>
+        <button onClick={handleAdd}>+ 1</button>
+      </div>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
+}
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired,
+}
+
+export default CounterApp;
+
---
+++ 03.counter-app/src/PrimeraApp.js
@@ -0,0 +1,34 @@
+// import React from "react"; //Descomentar si aparece error
+// import { Fragment } from "react";
+import { PropTypes } from "prop-types";
+
+const PrimeraApp = ({ saludo, subtitulo}) => {
+  // const saludo = 'Hola Mundo';
+  // const saludo = 123;
+  // const saludo = true; // Los valores booleanos no imprimen nada
+  // const saludo = [1,2,'Hola',4];
+  // const saludo = {
+  //   nombre: 'Julián',
+  //   edad: 32
+  // };
+
+
+  return (
+    <>
+      <h1>{saludo}</h1>
+      {/* Muestra un objeto formateado */}
+      {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+      <p>{subtitulo}</p>
+    </>
+  );
+}
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string,
+}
+
+PrimeraApp.defaultProps = {
+  subtitulo: 'Mi primera app React'
+}
+
+export default PrimeraApp;
